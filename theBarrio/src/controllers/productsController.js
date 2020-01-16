26c7014df33b01c7a71cfc7464c9081897e5a774
Felipe Rabu <felipe.rabuffetti@linio.com>
2020-01-15 23:02:14 -0300

    Hice funcionar el boton para borrar un producto

--- theBarrio/src/controllers/productsController.js
+++ theBarrio/src/controllers/productsController.js
@@ -17,18 +17,18 @@ const controller = {
     },
 
 
-    // Detail - Detail from one product
+    // Detail - Detail of one product
     detail: (req, res) => {
         let idURL = req.params.productId 
         res.render('detail', {arrayProducts, idURL})
     },
 
-    // Create - Form to create
+    // Create - Form to create products
     create: (req, res) => {
-        res.render('product-create-form') /* BORRAR ESTO */
+        res.render('product-create-form')
     },
     
-    // Create -  Method to store
+    // Create -  Method to store products
     store: (req, res) => {
 
         let contentProductsJSON = fs.readFileSync(productsFilePath, 'utf-8')
@@ -48,28 +48,38 @@ const controller = {
         fs.writeFileSync(productsFilePath, JSON.stringify(arrayProducts2, null, ' ')); 
 
         // Mensaje de éxito
-		res.send('¡Producto creado con éxitooooo!');
-        
+		res.send('¡Producto creado con éxitooooo!');        
     },
     
     // Update - Form to edit
     edit: (req, res) => {
-        // Do the magic
+        res.render('product-edit-form') //tengo que crear esta vista. Puede ser igual al de creacion pero hay que poner el id y los campos ya estan autocompletados ocn lo que tiene ahora
     },
+
     // Update - Method to update
     update: (req, res) => {
-        // Do the magic
+        let updatedProducts = arrayProducts.map (function(oneProduct) {
+            if (oneProduct.id == Number(req.params.productId)) {
+                return oneProduct == req.body; //esta mal esto pero es algo asi
+            }             
+        })
+        fs.writeFileSync(productsFilePath, JSON.stringify(updatedProducts, null, ' '));
+        res.send('Editaste el producto con id: ' + req.params.productId);
     },
+
     // Delete - Delete one product from DB
     destroy : (req, res) => {
 
-        console.log("======================>"+req.params.productId)
-        
+        console.log("El id de la URL es " + req.params.productId + " y es del tipo " + typeof Number(req.params.productId))
+
         let newProducts = arrayProducts.filter (function(oneProduct) {
-            oneProduct.id =! req.params.productId
+            console.log("El id del array es " + oneProduct.id + " y es del tipo " + typeof oneProduct.id);
+            console.log(oneProduct.id == Number(req.params.productId));
+            return oneProduct.id != Number(req.params.productId);              
         })
 
-        fs.writeFileSync(productsFilePath, JSON.stringify(newProducts)) 
+        fs.writeFileSync(productsFilePath, JSON.stringify(newProducts, null, ' '));
+        res.send('Borraste el producto con id: ' + req.params.productId);
     }
 };
 module.exports = controller;
