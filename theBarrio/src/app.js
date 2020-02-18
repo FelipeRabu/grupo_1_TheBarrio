a938ce58f03eebc52dfdd26909b4bbff736ef73f
carlosBA123 <suarezcar1990mail.com>
2020-02-17 23:57:34 -0300

    cambios en footer y modificaciones para session

--- theBarrio/src/app.js
+++ theBarrio/src/app.js
@@ -5,6 +5,7 @@ const express = require('express');
 const logger = require('morgan');
 const path = require('path');
 const methodOverride = require('method-override');  // requerir para editar lo que registramos en el formulario (A)
+const session = require('express-session');
 // ************ express() - (don't touch) ************
 const app = express();
 
@@ -14,6 +15,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(cookieParser());
+app.use(session({secret: 'secreto!!!'}));
 
 // ************ Template Engine - (don't touch) ************
 app.set('view engine', 'ejs');
